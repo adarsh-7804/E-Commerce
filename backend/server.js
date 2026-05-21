f0d705f06e797474d07d3795fcafa30285a77d43
adarsh-7804 <mradarshpate993l@gmail.com>
2026-05-21 17:01:02 +0530

    Changed frontend URL

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ app.use(express.json());
 // }));
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    origin: [
+        process.env.FRONTEND_URL,
+        'http://localhost:5173',
+        'https://e-commerce-seven-henna-75.vercel.app'
+    ].filter(Boolean),
     credentials: true,
 }));
 
