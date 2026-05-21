e88f84c5b9e9dd48a4b5f71735eec67dcb2ee3a5
adarsh-7804 <mradarshpate993l@gmail.com>
2026-05-21 13:08:43 +0530

    Changes CORS code and add social auth routes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,17 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors({
+//     origin: ['http://localhost:5173', 'http://localhost:5174'],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require("./routes/userRoutes");
@@ -19,6 +29,8 @@ const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const addressRoutes = require("./routes/addressRoutes");
+const socialAuthRoutes = require('./routes/socialAuthRoutes');
+
 
 app.use('/api/auth', authRoutes);
 app.use("/api/users", userRoutes);
@@ -26,6 +38,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/addresses", addressRoutes);
+app.use('/api/auth', socialAuthRoutes);
 
 
 app.get('/', (req, res) => {
